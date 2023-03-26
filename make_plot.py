7bab9968f7f8020c3d56fb9bcee346f7fe229c8e
Danielle Van Boxel <the.dvb@gmail.com>
2023-03-26 12:17:32 -0700

    make_plot.py

--- make_plot.py
+++ make_plot.py
@@ -8,11 +8,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-f','--dfile', default='results/res_all.csv', help='Results output file to read')
 parser.add_argument('-o','--ofile', default='results/pres_', help='Plots prepend name')
 parser.add_argument('-c','--cname', default='RMSE', help='Variable name to plot')
+parser.add_argument('-l','--log', default=False, action='store_true', help='Plot on log scale')
 
 args = parser.parse_args()
 dfile = args.dfile
 ofile = args.ofile
 cname = args.cname
+log = args.log
 
 df = pd.read_csv(dfile)
 
@@ -34,6 +36,8 @@ g = sns.catplot(x="Model", y=cname,
                 col_wrap=5,
                 split=True, inner='stick')
 #g.set_xticklabels(rotation=20)
+if log:
+    plt.yscale('log')
 plt.tight_layout(w_pad=1)
 plt.savefig(ofile + 'violin_results.png')
 plt.close()
@@ -46,11 +50,33 @@ g = sns.catplot(x="Model", y=cname,
                 col_wrap=5
                )
 #g.set_xticklabels(rotation=20)
+if log:
+    plt.yscale('log')
 plt.tight_layout(w_pad=1)
 plt.savefig(ofile + 'results.png')
+plt.close()
+
 
 # print table
 M = df.groupby('Dataset').mean()
 S = df.groupby('Dataset').std()
 print(M)
 print(S)
+
+# normalized boxplot
+mins = df2.groupby('Dataset').min()
+mins[f'min {cname}'] = mins[cname]
+df2 = df2.join(mins[f'min {cname}'], on='Dataset')
+df2[f'Rel {cname}'] = df2[cname] / df2[f'min {cname}']
+
+g = sns.boxplot(x='Model', y=f'Rel {cname}', data=df2)
+if log:
+    plt.yscale('log')
+plt.title(f'Relative {cname} Across All Data sets')
+if cname == 'RMSE':
+    plt.ylim(1,10) # beyond 10x worse isn't worth reporting
+g.grid(axis='y', which='both')
+plt.tight_layout(w_pad=1)
+plt.savefig(ofile + 'box_results.png')
+plt.close()
+
